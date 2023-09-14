7885cb9f87ab47b47f6078d4a5da95d830b1abd0
brazon84 <abg.brazon@gmail.com>
2023-09-14 14:18:23 -0400

    mejor

--- src/components/signUp/SignUp.jsx
+++ src/components/signUp/SignUp.jsx
@@ -1,106 +1,117 @@
 import React, { useState } from 'react';
-import { GoogleLogin } from 'react-google-login';
-import axios from 'axios';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+
 import { Link } from 'react-router-dom';
+import jwtDecode from "jwt-decode";
+
 
 const SignUp = () => {
-    const clientID = "345300445401-ffcd5tcp6mr0jcco4igp1geuu4bhqtqj.apps.googleusercontent.com";
-    const [user, setUser] = useState(null);
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-        firstName: '',
-        lastName: '',
-        country: '',
+    const [data, setData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        Photo: "",
+        terms: false,
     });
+    const [registrationStatus, setRegistrationStatus] = useState(null);
 
-    const createUser = async () => {
-        try {
-            await axios.post('http://localhost:8000/users', formData);
-        } catch (error) {
-            console.log(error);
+    const handleChangeData = (e) => {
+        const { name, value } = e.target;
+        setData(prevData => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const userData = { ...data }
+        if (data.terms) {
+            const requestData = {
+                name: data.firstName + " " + data.lastName,
+                email: data.email,
+                password: data.password,
+
+            };
+
+
+            try {
+                const response = await fetch.post("http://localhost:8000/users", { ...requestData });
+                console.log("Datos de registro:", response.data);
+                Swal.fire({
+                    icon: "success",
+                    title: "Register in!",
+                    text: `Welcome, ${requestData.name}! You have successfully registered.`,
+                });
+
+            } catch (error) {
+                console.error("Error al registrar:", error);
+                Swal.fire({
+                    icon: "error",
+                    title: "Fail Register!",
+                    text: ` ${error.response.data.message}!`,
+                });
+
+                setRegistrationStatus("error");
+            }
         }
     };
 
-    const onSuccess = async (response) => {
-        console.log('Google login success:', response);
-        setUser(response.profileObj);
-        const userData = {
-            email: response.profileObj.email,
-            firstName: response.profileObj.givenName,
-            lastName: response.profileObj.familyName,
-        };
-        try {
-            
-            await axios.post('http://localhost:8000/users', userData);
-            console.log('User registered successfully.');
-        } catch (error) {
-            console.error('Error registering user:', error);
-        }
-    }
-    
-    const onFailure = (response) => {
-        console.log('Google login failure:', response);
-    }
-    
-    const handleSignUp = async (event) => {
-        event.preventDefault();
-
-        try {
-            await createUser(formData);
-            setFormData({
-                email: '',
-                password: '',
-                firstName: '',
-                lastName: '',
-                country: '',
-            });
-        } catch (error) {
-            console.error('Error al crear el usuario:', error);
+    const renderAlert = () => {
+        if (registrationStatus === "success") {
+            return <div className="alert alert-success">Usuario registrado con éxito.</div>;
+        } else if (registrationStatus === "error") {
+            return <div className="alert alert-danger">Error al registrar el usuario.</div>;
         }
     };
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
-    };
 
     return (
-        <div>
-            <div className="d-flex justify-content-center">
-                <h1 className="fs-1 fw-bold">SignUp</h1>
-            </div>
-            <div className='d-flex justify-content-center'>
-                {!user ? (
-                    <div className='btn'>
+        <form
+            className="form d-flex justify-content-center align-items-center flex-column"
+            
+            onSubmit={handleSubmit}
+        >
+            <div className=' d-flex justify-content-center flex-column align-items-center text-center'>
+                <h2 className='d-flex justify-content-center mb-5 fw-bold'> Sign Up </h2>
+
+                <div className="d-flex justify-content-center">
+                    <GoogleOAuthProvider clientId="345300445401-ffcd5tcp6mr0jcco4igp1geuu4bhqtqj.apps.googleusercontent.com">
                         <GoogleLogin
-                            clientId={clientID}
-                            onFailure ={onFailure}
-                            onSuccess={onSuccess}
-                            buttonText="Continue with Google"
+                            onSuccess={(CredentialResponse) => {
+                                const infoUser = jwtDecode(CredentialResponse.credential);
+                                setData({
+                                    firstName: infoUser.given_name,
+                                    lastName: infoUser.family_name,
+                                    email: infoUser.email,
+                                    password: "jose1234",
+                                });
+                                Swal.fire({
+                                    icon: "success",
+                                    title: "Register in!",
+                                });
+                            }}
+                            onError={() => {
+                                console.log("Login Failed");
+                                Swal.fire({
+                                    title: "Something went wrong!",
+                                    icon: "error",
+                                });
+                            }}
                         />
-                    </div>
-                ) : (
-                    <div className="profile">
-                        <img src={user.imageUrl} alt="Profile" />
-                        <h3>{user.name}</h3>
-                        <button onClick={handleLogout}>Logout</button>
-                    </div>
-                )}
-            </div>
-            <div className='justify-content-center d-flex align-items-center'>
-                <div className="text-center">
-                    <form className='d-flex justify-content-center flex-column gap-3' onSubmit={handleSignUp}>
+                    </GoogleOAuthProvider>
+                </div>
+                <div className='justify-content-center d-flex align-items-center'>
+                    <div className="text-center">
                         <div className='d-flex justify-content-center flex-column'>
                             <label>Email:</label>
                             <input
                                 type="email"
                                 name="email"
-                                value={formData.email}
-                                onChange={handleInputChange}
+                                value={data.email}
+                                onChange={handleChangeData}
                             />
                         </div>
                         <div className='d-flex justify-content-center flex-column'>
@@ -108,8 +119,8 @@ const SignUp = () => {
                             <input
                                 type="password"
                                 name="password"
-                                value={formData.password}
-                                onChange={handleInputChange}
+                                value={data.password}
+                                onChange={handleChangeData}
                             />
                         </div>
                         <div className='d-flex justify-content-center flex-column'>
@@ -117,8 +128,8 @@ const SignUp = () => {
                             <input
                                 type="text"
                                 name="firstName"
-                                value={formData.firstName}
-                                onChange={handleInputChange}
+                                value={data.firstName}
+                                onChange={handleChangeData}
                             />
                         </div>
                         <div className='d-flex justify-content-center flex-column'>
@@ -126,35 +137,34 @@ const SignUp = () => {
                             <input
                                 type="text"
                                 name="lastName"
-                                value={formData.lastName}
-                                onChange={handleInputChange}
+                                value={data.lastName}
+                                onChange={handleChangeData}
                             />
                         </div>
-                        <label for="country">Country</label>
-                        <select name="country" id="country" value={formData.country}  onChange={handleInputChange}>
-                          <option value="chile">Chile</option>
+                        <label htmlFor="country">Country</label>
+                        <select name="country" id="country" value={data.country} onChange={handleChangeData}>  // Cambiado de formData.country a data.country
+                            <option value="chile">Chile</option>
                             <option value="ecuador">Ecuador</option>
                             <option value="argentina">Argentina</option>
                             <option value="venezuela">Venezuela</option>
                         </select>
-                        <button type="submit" className="btn btn-primary">Sign Up</button>
-                    </form>
+                        <div>
+                        <button type="submit" className=" mt-5 btn btn-primary">Sign Up</button>
+                        </div>
+                    </div>
                 </div>
+                <div className="d-flex justify-content-center gap-3 mt-5">
+                    <Link to="/">
+                        <button type="button" className="btn btn-secondary">Close</button>
+                    </Link>
+                    <Link to="/users/login">
+                        <button type="button" className="btn btn-primary">Sign In</button>
+                    </Link>
+                </div>
+
             </div>
-            <div className="d-flex justify-content-center gap-3 mt-5">
-                <Link to="/">
-                    <button type="button" className="btn btn-secondary">Close</button>
-                </Link>
-                <Link to="/users/login">
-                    <button type="button" className="btn btn-primary">Sign In</button>
-                </Link>
-            </div>
-        </div>
+        </form >
     );
 };
 
 export default SignUp;
-
-
-
-mytinerary-JoseBrazon
